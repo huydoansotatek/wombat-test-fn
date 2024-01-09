68eafb6ab05601e2df6599b2086a88d6606571b4
huy.doan <huy.nguyen8@sotatek.com>
2024-01-09 16:08:43 +0700

    1

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,12 +1,5 @@
 import React, { useRef, useState } from "react";
-import {
-  AppBar,
-  Box,
-  MenuItem,
-  MenuList,
-  Popover,
-  Toolbar,
-} from "@mui/material";
+import { AppBar, Box, MenuItem, MenuList, Popover, Toolbar } from "@mui/material";
 import ROUTERS_PATHS from "consts/router-paths";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
@@ -30,28 +23,12 @@ const Header = () => {
   return (
     <AppBar className={styles.appBar}>
       <Toolbar className={styles.toolBar}>
-        <Box className={styles.boxLogo}>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>1</Link>
-          <Link to={ROUTERS_PATHS.NOT_FOUND}>2</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>3</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>4</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>5</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>6</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>7</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>8</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>9</Link>
-          <Link to={ROUTERS_PATHS.DASHBOARD}>10</Link>
-        </Box>
 
-        <Box
-          sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-          ref={anchorElRef}
-          onClick={(e) => setOpenMenu(true)}
-        >
+        <Box sx={{ display: "flex", alignItems: "center", cursor: "pointer" }} ref={anchorElRef} onClick={(e) => setOpenMenu(true)}>
           <Icons.DefaultAvatarIcon />
           <div className={styles.textContainer}>
-            <p className={styles.userName}>Stephen Strange</p>
-            <p className={styles.userRole}>Doctor</p>
+            <p className={styles.userName}>Wombat exchange app</p>
+            <p className={styles.userRole}>Huy doan</p>
           </div>
         </Box>
         <Popover
